fb3a27d9332bda567fe197b2cf83571326ec7da1
Chirag30Sharma <chirag30sharma@gmail.com>
2024-06-16 07:37:37 +0530

    chore: Update routes in App.jsx for buyer and cluster pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,20 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Buyer from '../pages/Buyer';
+
 import About from '../pages/Buyer_folder/About';
 import Home from '../pages/Buyer_folder/Home';
 import Products from '../pages/Buyer_folder/Products';
-import Buyer from '../pages/buyer';
+import Buyer from '../pages/Buyer';
 import Cart from '../pages/Buyer_folder/Cart';
-import Master_admin from '../pages/Master_admin'
-
+import Master_admin from '../pages/Master_admin';
 import ImageUploader from '../pages/ImageUploader';
 import Login from '../pages/Login';
-import AuthForm from '../src/components/Authform';
+import AuthForm from '../components/Authform';
 import Subadmin from '../pages/Subadmin';
-
-import Login from '../pages/Login';
-import ImageUploader from '../pages/ImageUploader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
 
   const handleLogin = () => {
     setIsLoggedIn(true);
@@ -28,34 +23,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/buyer" element={<Buyer />}></Route> */}
-        <Route path='/buyer/' element={<Buyer />}></Route>
-        <Route path='/cluster/' element={isLoggedIn ? <Subadmin /> : <AuthForm onLogin={handleLogin} />}></Route>
-        <Route path='/buyer/home' element={<Home />}></Route>
-        <Route path='/buyer/about' element={<About />}></Route>
-        <Route path='/buyer/products' element={<Products />}></Route>
-        <Route path='/buyer/cart' element={<Cart />}></Route>
-        <Route path="client_admin" element={<ImageUploader />} />
-        <Route path="admin" element={<Master_admin />} />
-        <Route path="admin/login" element={<Login />} />
+        <Route path="/buyer" element={<Buyer />} />
+        <Route path="/cluster" element={isLoggedIn ? <Subadmin /> : <AuthForm onLogin={handleLogin} />} />
+        <Route path="/buyer/home" element={<Home />} />
+        <Route path="/buyer/about" element={<About />} />
+        <Route path="/buyer/products" element={<Products />} />
+        <Route path="/buyer/cart" element={<Cart />} />
+        <Route path="/client_admin" element={<ImageUploader />} />
+        <Route path="/admin" element={<Master_admin />} />
+        <Route path="/admin/login" element={<Login />} />
       </Routes>
-
-  return (
-    <BrowserRouter>      
-    <Routes>
-      {/* <Route path="/buyer" element={<Buyer />}></Route>   */}
-      <Route path='/buyer/' element={<Buyer/>}></Route>   
-      <Route path='/buyer/home' element={<Home/>}></Route> 
-      <Route path='/buyer/about' element={<About/>}></Route>   
-      <Route path='/buyer/products' element={<Products/>}></Route>   
-      <Route path='/buyer/cart' element={<Cart/>}></Route>   
-      
-      <Route path="client_admin" element={<ImageUploader/>} />
-      <Route path="admin" element={<Master_admin/>} />
-      <Route path="admin/login" element={<Login/>} />
-    </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
